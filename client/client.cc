ae116b915fc07931af8fcf8587c5a2a50919a232
zhangke <zhangke960808@163.com>
2020-06-21 14:55:40 +0800

    client添加地址解析

--- client/client.cc
+++ client/client.cc
@@ -1,5 +1,6 @@
 // Copyright [2020] zhangke
 
+#include <muduo/base/Logging.h>
 #include <muduo/net/InetAddress.h>
 #include <unistd.h>
 
@@ -9,10 +10,10 @@
 #include "client/proxy_client.h"
 
 void PrintUsage(const char *command) {
-  std::cout << "Usage:" << command << " -s server_ip"
+  std::cout << "Usage:" << command << " -s server_address"
             << " -p server_port"
             << " -t transfer_port"
-            << " -S proxy_server_ip"
+            << " -S proxy_server_address"
             << " -P proxy_server_port"
             << " -h help" << std::endl;
 }
@@ -79,9 +80,18 @@ int main(int argc, char *argv[]) {
     PrintUsage(argv[0]);
     exit(1);
   }
-  muduo::net::InetAddress server_address(server_address_p, server_port);
-  muduo::net::InetAddress proxy_server_address(proxy_server_address_p,
-                                               proxy_server_port);
+  muduo::net::InetAddress server_address(server_port);
+  bool ret =
+      muduo::net::InetAddress::resolve(server_address_p, &server_address);
+  if (!ret) {
+    LOG_FATAL << "resolve:" << server_address_p << " failed";
+  }
+  muduo::net::InetAddress proxy_server_address(proxy_server_port);
+  ret = muduo::net::InetAddress::resolve(proxy_server_address_p,
+                                         &proxy_server_address);
+  if (!ret) {
+    LOG_FATAL << "resolve:" << proxy_server_address_p << " failed";
+  }
   muduo::net::EventLoopThread event_loop_thread;
   muduo::net::EventLoop *loop = event_loop_thread.startLoop();
   std::shared_ptr<ProxyClient> proxy_client(std::make_shared<ProxyClient>(
